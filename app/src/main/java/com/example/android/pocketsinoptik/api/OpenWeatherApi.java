8a6288092d93c94c5fdbb935a48939680b4cd4ff
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-09 20:36:34 +0200

    design created

---
+++ app/src/main/java/com/example/android/pocketsinoptik/api/OpenWeatherApi.java
@@ -0,0 +1,8 @@
+package com.example.android.pocketsinoptik.api;
+
+/**
+ * Created by fbrsw on 09.11.2017.
+ */
+
+public interface OpenWeatherApi {
+}
---
+++ app/src/main/java/com/example/android/pocketsinoptik/views/BaseView.java
@@ -0,0 +1,13 @@
+package com.example.android.pocketsinoptik.views;
+
+import android.content.Context;
+
+/**
+ * Created by fbrsw on 09.11.2017.
+ */
+
+public interface BaseView {
+
+    Context getContext();
+
+}
