bd17e50bfff211d185c00f0865a53f9b1aa82bb8
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-09 22:27:08 +0200

    constant class created

---
+++ app/src/main/java/com/example/android/pocketsinoptik/Constants.java
@@ -0,0 +1,16 @@
+package com.example.android.pocketsinoptik;
+
+/**
+ * Created by fbrsw on 09.11.2017.
+ */
+
+public class Constants {
+    // http://api.openweathermap.org/data/2.5/weather?q=Haisyn&APPID=9ee593d07cae32e2c4f778eaa5989d9e&units=metric
+    // http://api.openweathermap.org/data/2.5/forecast?q=London,us&APPID=9ee593d07cae32e2c4f778eaa5989d9e&units=metric
+    public static final String OPEN_WEATHER_API = "http://api.openweathermap.org/data/2.5/";
+    public static final String OPEN_WEATHER_API_KEY = "9ee593d07cae32e2c4f778eaa5989d9e";
+    public static final String CELSIUS = "metric";
+    public static final String ONE_DAY = "weather";
+    public static final String FIVE_DAYS = "forecast";
+
+}
