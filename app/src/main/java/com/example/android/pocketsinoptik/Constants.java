a3cf1110546cfc459067ed069b0513fea64eed1c
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-10 21:06:04 +0200

    creathed class for constants

--- app/src/main/java/com/example/android/pocketsinoptik/Constants.java
+++ app/src/main/java/com/example/android/pocketsinoptik/Constants.java
@@ -10,7 +10,10 @@ public class Constants {
     public static final String OPEN_WEATHER_API = "http://api.openweathermap.org/data/2.5/";
     public static final String OPEN_WEATHER_API_KEY = "9ee593d07cae32e2c4f778eaa5989d9e";
     public static final String CELSIUS = "metric";
-    public static final String ONE_DAY = "weather";
-    public static final String FIVE_DAYS = "forecast";
+    public static final String ONE_DAY_WEATHER = "weather";
+    public static final String FIVE_DAYS_FORECAST = "forecast";
+    public static final String SIXTEEN_DAYS_FORECAST = "forecast/daily";
+    public static final String DAYS_COUNT = "16";
 
+    public static final String EXTRA_POSITION = "position";
 }
