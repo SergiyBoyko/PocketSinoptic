70354226a7a62287342723d154f4370d000106b7
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-11 23:39:54 +0200

    updated constants

--- app/src/main/java/com/example/android/pocketsinoptik/Constants.java
+++ app/src/main/java/com/example/android/pocketsinoptik/Constants.java
@@ -8,7 +8,8 @@ public class Constants {
     // http://api.openweathermap.org/data/2.5/weather?q=Haisyn&APPID=9ee593d07cae32e2c4f778eaa5989d9e&units=metric
     // http://api.openweathermap.org/data/2.5/forecast?q=London,us&APPID=9ee593d07cae32e2c4f778eaa5989d9e&units=metric
     public static final String OPEN_WEATHER_API = "http://api.openweathermap.org/data/2.5/";
-    public static final String OPEN_WEATHER_API_KEY = "9ee593d07cae32e2c4f778eaa5989d9e";
+//    public static final String OPEN_WEATHER_API_KEY = "9ee593d07cae32e2c4f778eaa5989d9e"; // my
+    public static final String OPEN_WEATHER_API_KEY = "d634bba455936939a1ac5189b96d1830"; // my bro
     public static final String CELSIUS = "metric";
     public static final String ONE_DAY_WEATHER = "weather";
     public static final String FIVE_DAYS_FORECAST = "forecast";
@@ -16,4 +17,19 @@ public class Constants {
     public static final String DAYS_COUNT = "16";
 
     public static final String EXTRA_POSITION = "position";
+
+    public static final String DAY = "day";
+    public static final String NIGHT = "night";
+
+    public static final String DATE_FORMAT = "dd.MM.yyyy";
+    public static final String CELSIUS_ENDING = "°C";
+
+    public static final String SUNNY_DAY_WEATHER = "Sunny";
+    public static final String CLEAR_NIGHT_WEATHER = "Clear night";
+    public static final String THUNDER_WEATHER = "Thunder";
+    public static final String DRIZZLE_WEATHER = "Drizzle";
+    public static final String FOGGY_WEATHER = "Foggy";
+    public static final String CLOUDY_WEATHER = "Cloudy";
+    public static final String SNOWY_WEATHER = "Snowy";
+    public static final String RAINY_WEATHER = "Rainy";
 }
